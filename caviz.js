ef45372d30b48cad3af31bc7e5e636e176a92c72
Jonas Almeida <jalmeida@mathbiol.org>
2019-05-29 14:48:00 -0600

    Update caviz.js

--- caviz.js
+++ caviz.js
@@ -1,1 +1,13 @@
-console.log('caviz.js loaded')
+console.log('caviz.js loaded');
+
+(function(){
+    let caviz={
+        created_at:Date()
+    }
+    if(typeof(define)!='undefined'){
+        define(caviz)
+    }
+    if(typeof(window)!='undefined'){
+        window.caviz=caviz
+    }
+})()
