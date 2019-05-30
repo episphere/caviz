5fdce9b8008d94343545ab887ee8f89140cb21f2
Jonas Almeida <jalmeida@mathbiol.org>
2019-05-29 18:46:16 -0600

    seer api key management

--- caviz.js
+++ caviz.js
@@ -1,16 +1,26 @@
 console.log('caviz.js loaded');
 
-(function(){
+(function(){ // caviz in the scope of an anonymous function so it wont contaminate root if required
     let caviz={
         created_at:Date()
     }
 
-    caviz.getSeerKey=id=>{ // get/provide SEER key from input elelment with given id
-        id=id||'seerKeyInput'
-        let ip = document.getElementById(id)
-        
+    caviz.seerStatus = async function(k){ // k is api key
+        k = k||(function(){
+            if(typeof(localStorage)=="object"){ // update from localStorage if available
+                if(localStorage.seerKey){
+                    caviz.seerKey=localStorage.seerKey
+                }
+            }
+            return caviz.seerKey
+        })()
+        //return (await fetch('https://api.seer.cancer.gov/rest/staging/cs/02.05.50/schemas?api_key='+k)).json()
+        return (await fetch('https://api.seer.cancer.gov/rest/staging/cs/02.05.50/schemas',{
+            headers:{
+                'X-SEERAPI-Key':k
+            }
+        })).json()
     }
-
 
 
     if(typeof(define)!='undefined'){
