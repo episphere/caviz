c8f7b500e3bc38b8ca4987eb0927935d3d35512f
Jonas Almeida <jalmeida@mathbiol.org>
2019-11-11 15:58:53 -0500

    v1

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 exports.helloHttp = (req, res) => {
   console.log(req.originalUrl)
   res.header('Access-Control-Allow-Origin','*')
-  res.send(`Hello caViz at ${Date()}`);
+  res.send(`Hello caViz 1 at ${Date()}`);
 };
